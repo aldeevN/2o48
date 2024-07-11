508d2a3d5a7edb1a6a31714f663a093eec14095f
AldeevN <aldeev33@gmail.com>
2024-07-11 12:44:03 +0300

    hot fix

--- game-2048-frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/root/proj/2o48/game-2048-frontend/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- game-2048-frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2pAJZi6SwHABbzj3WgU3TLjQ48RL1BXv8GrdeFtpc2w=\"\n}"
